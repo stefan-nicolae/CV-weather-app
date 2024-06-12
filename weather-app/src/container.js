27a96d7b385ae2c18bf434e5ef52e29bd6f56af8
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2024-06-12 20:54:06 +0300

    update

--- weather-app/src/container.js
+++ weather-app/src/container.js
@@ -6,7 +6,6 @@ import { useEffect, useRef, useState } from "react";
 const APIkey = process.env.REACT_APP_APIkey
 const GEOkey = process.env.REACT_APP_GEOkey
 
-
 function getCurrentLatLong(callback, message) {
     let latitude, longitude
     navigator.geolocation.getCurrentPosition(position => {
